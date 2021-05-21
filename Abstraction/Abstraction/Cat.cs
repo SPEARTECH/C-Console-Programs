f2675044bb245c880288a438cdc94ac27b0e3dcc
SPEARTECH <70530918+SPEARTECH@users.noreply.github.com>
2021-05-21 10:56:14 -0400

    Add files via upload

---
+++ Abstraction/Abstraction/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Cat : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Hey Im a Cat!");
+        }
+    }
+}
---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            d1.AnimalSound();
+            d1.sleep();
+            Cat c1 = new Cat();
+            c1.AnimalSound();
+            Console.ReadLine();
+        }
+
+    }
+
+}
